ed07ca309d2113baca3a02f00f50c697fcb36728
Leah Ooi <leyao_@hotmail.com>
2018-01-07 17:58:07 -0500

    Added date/time display and background color with hex value corresponding to time of day

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Constants from './constants';
 import './App.css';
 
-class App extends Component {
+class ChromeStartup extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      curTime: '',
+      curDate: '',
+      bgColour: ''
+    };
+  }
+
+  componentDidMount() {
+    var dateOptions = { weekday: 'long', month: 'long', day: 'numeric'};
+    var timeOptions = { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+    setInterval( () => {
+      var time = new Date();
+      var timeInMs = time.getHours() * Constants.HOUR_IN_MS + time.getMinutes() * Constants.MINUTE_IN_MS +
+                     time.getSeconds() * Constants.SECOND_IN_MS + time.getMilliseconds();
+
+      this.setState({
+        curDate: time.toLocaleString('en-US', dateOptions),
+        curTime: time.toLocaleString('en-US', timeOptions),
+        bgColour: Math.round(timeInMs / Constants.DAY_IN_MS * Constants.MAX_HEX_VALUE).toString(16)
+      });
+    }, 1000);
+  }
+
+  getBgColour() {
+    return {
+      backgroundColor: '#' + this.state.bgColour,
+      height: '100vh'
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="bg-container" style={this.getBgColour()}>
+        <div className="display-text">
+          <p id="cur-date">{this.state.curDate}</p>
+          <p id="cur-time">{this.state.curTime}</p>
+          <p id="bg-colour">#{this.state.bgColour}</p>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default ChromeStartup;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    DAY_IN_MS: 86400000,
+    HOUR_IN_MS: 3600000,
+    MINUTE_IN_MS: 60000,
+    SECOND_IN_MS: 1000,
+    MAX_HEX_VALUE: 16777215
+});
